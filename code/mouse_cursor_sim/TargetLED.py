b9e2403ceb32f842e025454950fa9b25b30db576
westl5 <westl5@mcmaster.ca>
2025-01-17 12:36:09 -0500

    reorganized folders

---
+++ code/mouse_cursor_sim/TargetLED.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class TargetLED:
+    def __init__(self, shape, points:np.ndarray):
+        self.shape = shape
+        self.points = points
+        self.num_points = np.size(points)
+        
+        
+
+    def some_method(self):
+        # Method implementation
+        pass
